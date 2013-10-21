943ffd74d9a3eafc8c1d1e7e8dcc616df2106ec7
Jared Martin <jaredtmartin@gmail.com>
2013-10-20 22:28:37 -0600

    Included css file again

--- articles/xxxuser_urls.py
+++
@@ -1,6 +0,0 @@
-from django.conf.urls import patterns, url, include
-from articles.views import *
-
-urlpatterns = patterns('',
-    url(r'^(?P<username>[-\w]+)/(?P<pk>\d+)/$', UserUpdateView.as_view(template_name='articles/user_form.html'), name='user_update')
-)
